e6daac1c664073879003960cb845ecff87b6c581
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-08-01 03:06:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-//var playlist = new Object ({artistName: //'songTitle'});
+/*var playlist = new Object ({artistName: 'songTitle'});
   
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
@@ -11,4 +11,4 @@ function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }
-
+*/
